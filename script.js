4db5ca7014e6580383c8a6e5466dfab7fddd3b03
marcosg <marcos_a_garcia@yahoo.com.ar>
2023-03-21 16:25:47 -0300

    añadiendo mis primeros archivos en git

---
+++ script.js
@@ -0,0 +1,14 @@
+/*let numero = parseInt(prompt("ingrese un numero"))
+*/
+/*parseint, parsefloat, Number*/
+
+const nombres = ["mariano", "gaston", "pepe", "maria"]
+
+/*for(let i = 0; i < nombres.length; i++)
+{
+    console.log(nombres[i])
+}*/
+
+for(let nombre of nombres){
+    console.log(nombre)
+}
